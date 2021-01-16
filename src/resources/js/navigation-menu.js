bfe850b017a8e952f007b738011fad7c128991f3
Nysera <73109965+Nysera@users.noreply.github.com>
2021-01-17 09:05:31 +1000

    Refactored to create every element instead of using innerHTML

--- src/resources/js/navigation-menu.js
+++ src/resources/js/navigation-menu.js
@@ -1,23 +1,45 @@
 const navigationMenu = function() {
+    // nav container
     const navElement = document.createElement("nav");
     navElement.classList.add("nav");
+
+    // nav inner
     const navInner = document.createElement("div");
     navInner.classList.add("nav-inner")
-    navInner.innerHTML = `
-        <div></div>
-        <div class="nav-links">
-            <button>Home</button>
-            <button>Menu</button>
-            <button>Contact</button>
-        </div>
-        <div class="nav-footer">
-            <span>(03) 1538 1349</span>
-            <a>contact@aurora.com</a>
-        </div>
-    `;
     navElement.append(navInner);
+    
+    // empty div for flexbox
+    const emptyDiv = document.createElement("div");
+    navInner.append(emptyDiv);
 
-    // 
+    // div to store links in
+    const navInnerLinks = document.createElement("div");
+    navInnerLinks.classList.add("nav-links");
+    navInner.append(navInnerLinks);
+    for (let i= 0; i < 3; i++) {
+        const linkNames = ["home", "menu", "contact"];
+        const navInnerLinksButtons = document.createElement("button");
+        navInnerLinksButtons.innerHTML = linkNames[i];
+        navInnerLinks.append(navInnerLinksButtons);
+    }
+
+    // nav footer
+    const navFooter = document.createElement("div");
+    navFooter.classList.add("nav-footer");
+    navInner.append(navFooter);
+
+    // phone number
+    const navFooterPhone = document.createElement("span");
+    navFooterPhone.innerHTML = "(03) 1538 1349";
+    navFooter.append(navFooterPhone);
+
+    // email
+    const navFooterEmail = document.createElement("a");
+    navFooterEmail.setAttribute("href", "/");
+    navFooterEmail.innerHTML = "contact@aurora.com";
+    navFooter.append(navFooterEmail);
+
+    // append navigation menu to body
     document.body.append(navElement);
 };
 
