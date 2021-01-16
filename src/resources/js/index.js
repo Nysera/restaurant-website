981b5bd6086abef031375c39f03c5d31f77cb3e1
Nysera <73109965+Nysera@users.noreply.github.com>
2021-01-16 12:11:28 +1000

    Add logic to toggle menu

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,2 +1,26 @@
 // import CSS
 import "../css/styles.scss";
+import header from "../js/header.js";
+import navigationMenu from "../js/navigation-menu";
+
+const dom = (function() {
+    const initialize = function() {
+        header();
+        navigationMenu();
+        addEventDelegation();
+    };
+    const addEventDelegation = function() {
+        document.body.addEventListener("click", function(event){
+            if (event.target.classList.contains("menu-btn")) {
+                toggleMenu();
+            }
+        });
+    };
+    const toggleMenu = function () {
+        const body = document.body;
+        !body.classList.contains("menu-open") ? body.classList.add("menu-open") : body.classList.remove("menu-open")
+    };
+
+
+    initialize();
+}());
