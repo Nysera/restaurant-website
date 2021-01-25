fb013dbcd7f494b2d927ebf017e6df6072419432
Nysera <73109965+Nysera@users.noreply.github.com>
2021-01-25 14:28:00 +1000

    Change class which is used for changing to the menu page

--- src/resources/js/home.js
+++ src/resources/js/home.js
@@ -108,7 +108,7 @@ const home = function() {
     // menu section
     // menu container
     const menuContainer = document.createElement("div");
-    menuContainer.classList.add("menu");
+    menuContainer.classList.add("our-menu");
 
     // menu inner container
     const menuInnerContainer = document.createElement("div");
@@ -143,7 +143,7 @@ const home = function() {
 
     // menu button
     const menuButton = document.createElement("button");
-    menuButton.classList.add("btn", "btn-solid", "menu-page");
+    menuButton.classList.add("btn", "btn-solid", "menu-page-btn");
     menuInner.append(menuButton);
     const menuButtonSpan = document.createElement("span");
     menuButtonSpan.innerHTML = "View Menu";
